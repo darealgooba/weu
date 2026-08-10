5ea2b9e20e5db67a10dd8266dd44cd1fa6164227
darealgooba <ljafljaf123@gmail.com>
2026-08-10 15:45:21 -0500

    Increase inventory limit from 45 to 85

--- wm.lua
+++ wm.lua
@@ -12,7 +12,7 @@ local PlayerGui = Player:WaitForChild("PlayerGui")
 -- CONFIGURATION
 --==================================================
 
-local INVENTORY_LIMIT = 45
+local INVENTORY_LIMIT = 85
 
 local WRECK_ARRIVAL_DISTANCE = 7
 local SELLER_ARRIVAL_DISTANCE = 7
